50722d9c37fc108459f5f40a6b197a6191337c2a
makampos <matheus.campos@edu.unifil.br>
2024-02-20 18:48:40 -0300

    creating controller to expose endpoints to upload and download files.

--- Samples/WebApiSample/Controllers/AzureStorageController.cs
+++ Samples/WebApiSample/Controllers/AzureStorageController.cs
@@ -1,5 +1,7 @@
-using ManagedCode.Storage.Azure;
+using ManagedCode.Communication;
+using ManagedCode.Storage.Core.Models;
 using Microsoft.AspNetCore.Mvc;
+using WebApiSample.Interfaces;
 
 namespace WebApiSample.Controllers;
 
@@ -7,10 +9,43 @@ namespace WebApiSample.Controllers;
 [Route("api/[controller]")]
 public class AzureStorageController : Controller
 {
-    private readonly IAzureStorage _azureStorage;
+    private readonly IAzureStorageService _azureStorageService;
 
-    public AzureStorageController(IAzureStorage azureStorage)
+    public AzureStorageController(IAzureStorageService azureStorageService)
     {
-        _azureStorage = azureStorage;
+        _azureStorageService = azureStorageService;
+    }
+    
+    [HttpPost("upload")]
+    public async Task<ActionResult<Result<BlobMetadata>>> UploadFile(IFormFile formFile)
+    {
+        try
+        {
+            if (formFile == null || formFile.Length == 0)
+            {
+                return BadRequest("Invalid file.");
+            }
+
+            return Ok(await _azureStorageService.UploadFileAsync(formFile));
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, $"Internal server error:{e.Message}");
+        }
+    }
+
+    [HttpGet("download/{fileName}")]
+    public async Task<IActionResult> DownloadFile(string fileName)
+    {
+        try
+        {
+            var result = await _azureStorageService.DownloadFileAsync(fileName);
+
+            return File(result.Value.FileStream, "application/octet-stream", fileName);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, $"Internal server error: {e.Message}");
+        }
     }
 }
